b15acc3e63ca37b47e1abdf65aa60fc1928eb730
Uday <udaykumar.1997@gmail.com>
2019-06-09 02:39:41 +0530

    keras added

--- server_setup.sh
+++ server_setup.sh
@@ -1,7 +1,7 @@
 sudo apt update
 sudo apt install ubuntu-desktop gnome-panel gnome-settings-daemon gnome-terminal metacity nautilus vnc4server nvidia-384 -y
 nvidia-smi --loop=1
-sudo apt install python3-pip -y
+sudo apt install python3-pip keras -y
 sudo pip3 install opencv-python 
 sudo apt install python-matplotlib python-numpy python-pil python-scipy build-essential cython python-skimage python3-skimage -y
 cp xstartup ../.vnc/
