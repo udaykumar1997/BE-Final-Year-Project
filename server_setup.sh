9acbbe87e37ca22671aef88fbbf402fb29d6c377
Uday <udaykumar.1997@gmail.com>
2019-08-01 02:33:26 +0530

    license addition and first commit from atom

--- server_setup.sh
+++ server_setup.sh
@@ -1,3 +1,16 @@
+# @Date:   2019
+# @Last modified time: 2019
+# @Copyright:
+# Copyright 2019 Uday Kumar Adusumilli
+
+# Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. You may obtain a copy of the License at
+
+#	http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
+
+
+
 sudo apt update
 sudo apt install ubuntu-desktop gnome-panel gnome-settings-daemon gnome-terminal metacity nautilus vnc4server nvidia-384 -y
 nvidia-smi --loop=1
