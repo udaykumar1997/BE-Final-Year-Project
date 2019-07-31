bd62a73856bc92e1289ee5e51ee4dcc06d80087f
Uday <udaykumar.1997@gmail.com>
2019-07-31 13:09:03 +0530

    license change

--- server_setup.sh
+++ server_setup.sh
@@ -2,7 +2,7 @@ sudo apt update
 sudo apt install ubuntu-desktop gnome-panel gnome-settings-daemon gnome-terminal metacity nautilus vnc4server nvidia-384 -y
 nvidia-smi --loop=1
 sudo apt install python3-pip keras -y
-sudo pip3 install opencv-python 
+sudo pip3 install opencv-python
 sudo apt install python-matplotlib python-numpy python-pil python-scipy build-essential cython python-skimage python3-skimage -y
 cp xstartup ../.vnc/
 wget https://repo.anaconda.com/archive/Anaconda3-2019.03-Linux-x86_64.sh
